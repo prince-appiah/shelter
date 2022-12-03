afc8682fef391803ae8406be646a594043c2d70c
Prince Appiah <pappiah00@gmail.com>
2022-12-03 20:14:30 +0000

    Minor change

--- src/redux/auth/asyncActions.ts
+++ src/redux/auth/asyncActions.ts
@@ -1,72 +1,56 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import jwtDecode from "jwt-decode";
-import AuthApi, {
-  LoginFields,
-  OtpFields,
-  SignupFields,
-} from "services/auth.api";
+import AuthApi, { LoginFields, OtpFields, SignupFields } from "services/auth.api";
 import { IDecodedUser } from "typings";
 import { setCurrentUser } from "./authSlice";
 
-export const signupAction = createAsyncThunk(
-  "auth/signup",
-  async (data: SignupFields, thunk) => {
-    try {
-      const response = await AuthApi.signUp(data);
+export const signupAction = createAsyncThunk("auth/signup", async (data: SignupFields, thunk) => {
+  try {
+    const response = await AuthApi.signUp(data);
 
-      return response.data;
-    } catch (error) {
-      return thunk.rejectWithValue(error.response.data);
-    }
+    return response.data;
+  } catch (error) {
+    return thunk.rejectWithValue(error.response.data);
   }
-);
+});
 
-export const loginAction = createAsyncThunk(
-  "auth/login",
-  async ({ otp, email }: LoginFields, thunk) => {
-    try {
-      const response = await AuthApi.login({ email, otp });
+export const loginAction = createAsyncThunk("auth/login", async ({ otp, email }: LoginFields, thunk) => {
+  try {
+    const response = await AuthApi.login({ email, otp });
 
-      localStorage.setItem("token", response.data.token);
-      const decoded = (await jwtDecode(response.data.token)) as IDecodedUser;
-      thunk.dispatch(setCurrentUser(decoded));
+    localStorage.setItem("token", response.data.token);
+    const decoded = (await jwtDecode(response.data.token)) as IDecodedUser;
+    thunk.dispatch(setCurrentUser(decoded));
 
-      return response.data;
-    } catch (error) {
-      return thunk.rejectWithValue(error.response.data);
-    }
+    return response.data;
+  } catch (error) {
+    return thunk.rejectWithValue(error.response.data);
   }
-);
+});
 
-export const getOtpAction = createAsyncThunk(
-  "auth/getOtp",
-  async ({ email }: OtpFields, thunk) => {
-    try {
-      const response = await AuthApi.getOTP({ email });
+export const getOtpAction = createAsyncThunk("auth/getOtp", async ({ email }: OtpFields, thunk) => {
+  try {
+    const response = await AuthApi.getOTP({ email });
 
-      return response.data;
-    } catch (error) {
-      return thunk.rejectWithValue(error.response.data);
-    }
+    return response.data;
+  } catch (error) {
+    return thunk.rejectWithValue(error.response.data);
   }
-);
+});
 
-export const logoutAction = createAsyncThunk(
-  "auth/logout",
-  async (data, thunk) => {
-    try {
-      const response = await AuthApi.logout();
-      console.log("🚀 ~ response", response);
+export const logoutAction = createAsyncThunk("auth/logout", async (data, thunk) => {
+  try {
+    const response = await AuthApi.logout();
+    console.log("🚀 ~ response", response);
 
-      // if (response.status === 200) {
-      //   logoutUser();
-      //   window.location.pathname = LOGIN_ROUTE;
-      //   return;
-      // }
+    // if (response.status === 200) {
+    //   logoutUser();
+    //   window.location.pathname = LOGIN_ROUTE;
+    //   return;
+    // }
 
-      return response;
-    } catch (error) {
-      return thunk.rejectWithValue(error.response.data);
-    }
+    return response;
+  } catch (error) {
+    return thunk.rejectWithValue(error.response.data);
   }
-);
+});
